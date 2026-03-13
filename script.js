dc572c4e1fd930700b2eb67d24e140ae2f0395ef
Md Suhail <sha433415@gmail.com>
2026-03-13 23:18:03 +0530

    website main layout done basic html page also done need to updates too for now just outlay has created

---
+++ script.js
@@ -0,0 +1,37 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+function toggleSidebar(){
+
+sidebar.classList.toggle("active");
+overlay.classList.toggle("active");
+
+}
+
+function closeSidebar(){
+
+sidebar.classList.remove("active");
+overlay.classList.remove("active");
+
+}
+
+
+// DARK MODE
+
+document.getElementById("darkToggle").onclick = function(){
+
+document.body.classList.toggle("dark");
+
+}
+
+
+// DEMO CODE RUN
+
+function runCode(){
+
+let code = document.getElementById("code").value;
+
+document.getElementById("output").innerText =
+"Java requires backend compiler.\n\nYour code:\n\n" + code;
+
+}
