ee8bc578347b8c3a99e7b5d504b0a92b548b042f
Md Suhail <sha433415@gmail.com>
2026-03-14 14:52:54 +0530

    wroked on frontend of main page section implemnted dark mode

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ document.getElementById("output").innerText =
 "Java requires backend compiler.\n\nYour code:\n\n" + code;
 
 }
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+const question = item.querySelector(".faq-question");
+
+question.addEventListener("click", () => {
+
+faqItems.forEach(el => {
+if(el !== item){
+el.classList.remove("active");
+}
+});
+
+item.classList.toggle("active");
+
+});
+
+});
